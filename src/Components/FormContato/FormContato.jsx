0ba93d4d7bb52339c9255bb3c3457dda936233af
Estefani-prog <fannemonteiro95@gmail.com>
2022-03-22 19:29:36 -0300

    Página "Quem somos"

--- src/Components/FormContato/FormContato.jsx
+++ src/Components/FormContato/FormContato.jsx
@@ -6,7 +6,16 @@ export default () => {
     <>
        <main class="vh-100 ">
         <section class="container col-8">
-            <h1 class="display-3 mt-4">Entre em contato</h1>
+        <h1 class="display-3 mt-4">Entre em contato</h1>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome Completo" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Telefone</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(21) 99777-8888" />
+            </div>
+            
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Email</label>
                 <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com" />
---
+++ src/Pages/QuemSomos/QuemSomos.jsx
@@ -0,0 +1,13 @@
+import Header from "../../Components/Header/Header"
+import Footer from "../../Components/Footer/Footer"
+import QuemSomos from "../../Components/QuemSomos/QuemSomos"
+
+export default ()=>{
+    return(
+        <>
+        <Header></Header>
+        <QuemSomos></QuemSomos>
+        <Footer></Footer>
+        </>
+    )
+}
