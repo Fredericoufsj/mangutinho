0c53a605af530e46845022c0a6b31c9deac273f1
JessicaLuana <j.luaana@live.com>
2022-03-22 19:32:25 -0300

    atualização footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,34 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default ()=>{
-    return(
+import React from "react";
+import squad19 from "../../assets/img/recode2021-squad19.png";
+import './Footer.css';
+
+export default () => {
+    return (
         <>
-         <footer className="text-white text-center text-lg-start">        
-        <div class="text-center p-3 nav-cor">
-            &copy; 2022 Atividade em grupo - Recode Pro
-            <a href="">Projeto Mangut</a>
-        </div>       
-    </footer>
+
+            <footer className="d-flex justify-content-start align-self-baseline align-items-xxl-end footer1" data-aos="slide-up" data-aos-delay="50">
+                <div className="container">
+                    <div className="row d-lg-flex flex-column justify-content-lg-center">
+                        <div className="col-lg-auto text-primary d-xxl-flex justify-content-center align-content-end align-self-center align-items-xxl-end">
+                            <p className="text-center text-light d-lg-flex justify-content-lg-center align-items-lg-center footer2">
+                                <span className="footer3">
+                                    <br></br>
+                                    <em>"Qualidade de vida é viver daquilo que a natureza tem a oferecer"</em>
+                                    <br></br>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="col-auto d-flex flex-column flex-grow-0 justify-content-center align-content-around align-self-center flex-wrap align-items-lg-center">
+                            <img className="d-lg-flex justify-content-lg-center align-items-lg-center footer4" src={squad19} />
+                            <p className="text-center text-white d-flex d-xxl-flex justify-content-center align-items-center mx-auto justify-content-md-center align-items-md-center align-items-lg-center justify-content-xxl-center align-items-xxl-center footer5">Squad</p>
+                            <img src="https://www.recodepro.org.br/wp-content/uploads/2021/04/recodepro.png" />
+                             <div className="w-100">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
+
